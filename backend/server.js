5c5dbcd6b1221c1d6573de91a69d7a91a5109a4d
Ren Hoshizora <mthariqaryaputra1@gmail.com>
2024-12-26 01:49:49 +0700

    Fix to deploy api

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import orderRouter from "./routes/orderRoute.js";
 
 // App config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000; // Use the PORT from the environment or default to 4000
 
 // Middleware
 app.use(express.json());
@@ -26,9 +26,10 @@ app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
-    res.send("API Works")
-})
+    res.send("API Works");
+});
 
-app.listen(port, () => {
+// App listens on IP 0.0.0.0 and port from environment variable
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server running at http://localhost:${port}`);
-})
+});
